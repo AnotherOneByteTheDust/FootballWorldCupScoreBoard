a321ca284547118f3f643ad7a81a9dfba3c7cb47
Kevin Eliezer <kevineli.gp@gmail.com>
2021-08-09 23:10:15 +0100

    test: total score should be equals to 0

--- tests/integration/footballworldcup.int.test.ts
+++ tests/integration/footballworldcup.int.test.ts
@@ -6,13 +6,23 @@ const scoreboard = new FootballWorldCupScoreBoard();
 describe('Competition Score Board', () => {
 
   describe('Start a new game', () => {
-    it('Should be able to start a new game', () => {
+    it('should be able to start a new game', () => {
       const hostTeam = new Team("Alemania");
       const awayTeam = new Team("Francia");
 
       const game = scoreboard.newGame(hostTeam, awayTeam);
 
       expect(game).toBeDefined();
+    })
+
+    it('should have total score equals to 0', () => {
+      const hostTeam = new Team("Alemania");
+      const awayTeam = new Team("Francia");
+
+      const game = scoreboard.newGame(hostTeam, awayTeam);
+      const totalScore = scoreboard.getMatchTotalScore(game);
+
+      expect(totalScore).toEqual(0);
     })
   })
 })
